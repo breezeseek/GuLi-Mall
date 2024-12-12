80988bf066b22a46f5acf41e99ad1770111ef7a2
ppj <1043719676@qq.com>
2024-12-12 11:09:38 +0800

    逆向工程搭建，配置，微服务基础CRUD

--- mall-product/src/test/java/com/learn/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/learn/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,22 @@
 package com.learn.mall.product;
 
+import com.learn.mall.product.entity.BrandEntity;
+import com.learn.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+    @Resource
+    private BrandService brandService;
+
     @Test
     void contextLoads() {
+        BrandEntity byId = brandService.getById(1L);
+        System.out.println(byId);
     }
 
 }
